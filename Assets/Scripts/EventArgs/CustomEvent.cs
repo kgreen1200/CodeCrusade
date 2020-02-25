46480a5db49a4a5112689b622ba8cfe6642e4d04
kevnh <khuynhn@live.com>
2020-02-25 11:24:27 -0800

    Modified Input System to use UnityEvents instead of EventHandler

---
+++ Assets/Scripts/EventArgs/CustomEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Custom UnityEvents with custom types
+public class CustomEvent<T> : UnityEvent<T>
+{
+
+}
