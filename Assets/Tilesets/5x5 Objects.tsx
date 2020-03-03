b5dac2808fa1bba421d0cd4caae8ef4ce1e923ac
Kevin Green <k_green5@u.pacific.edu>
2020-03-02 18:05:45 -0800

    Created Tilesets for the houses and added them

---
+++ Assets/Tilesets/5x5 Objects.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="5x5 Objects" tilewidth="80" tileheight="80" tilecount="8" columns="8">
+ <image source="../Art/gfx/5x5 Objects.png" width="640" height="80"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="5">
+   <object id="5" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="45" width="67" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="7">
+   <object id="6" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="45" width="66" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
