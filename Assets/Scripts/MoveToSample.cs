816c42f419c81e169a8597ddfec7230f0a5a780e
Kevin Green <k_green5@u.pacific.edu>
2020-03-26 11:21:23 -0700

    Modified Some Project Settings, added title screen that will get you to the overworld debug if shift is hit 3 times.

---
+++ Assets/Scripts/MoveToSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToSample : MonoBehaviour
+{
+    private int pressCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            pressCount = pressCount + 1;
+            Debug.Log("PressCount is now " + pressCount);
+            if (pressCount == 3) {
+                Debug.Log("Loading new scene");
+                SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            }
+        }
+    }
+}
