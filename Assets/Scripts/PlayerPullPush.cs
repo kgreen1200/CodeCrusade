d632a04a065c9ee810a8dc0fda52985acb8d2d81
Kevin Green <k_green5@u.pacific.edu>
2020-03-31 08:05:58 -0700

    Created Box that the player can push.
    Created "Box" that the player can push around in the environment. The player can run into the box and it will push the box.
    =========================
    Note: To change the friction of the box, select the box and change the "Linear Drag"on the Rigidbody 2D

---
+++ Assets/Scripts/PlayerPullPush.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPullPush : MonoBehaviour
+{
+
+
+    public float pushPower = 0.2F;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("Hit");
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.z < -0.3f || hit.transform.name != "Box")
+            return;
+
+        Debug.Log("Player has hit box.");
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
+    }
+}
