4c6d7ada83e5a2f069f707b9fb9d946f3d5c296d
Kevin Green <k_green5@u.pacific.edu>
2020-03-31 11:52:26 -0700

    Added basics for scoring system into the game. More will be coming soon as it gets more complex.

---
+++ Assets/Scripts/GameScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour
+{
+    public int scoreDecrement = 1;
+    public int maxScore = 5000;
+    private int score;
+    private float timer;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        score = maxScore;
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime; // Add the change in time;
+        if (timer > 5f && score != 0)
+        {
+            score -= scoreDecrement;
+            scoreText.text = score.ToString();
+            timer = 0;
+        }
+    }
+}
