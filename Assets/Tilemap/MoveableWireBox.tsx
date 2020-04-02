382371248140bb4192c3012a2fccada6a8362d1b
Ian Higa <finn2252000@gmail.com>
2020-04-02 10:31:43 -0700

    Added Tilesets
    MoveableWireBox
    OpenableDoor

---
+++ Assets/Tilemap/MoveableWireBox.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="MoveableWire" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="../../../../../OneDrive/Code Crusade Resources/Newgfx/BoxSheet.png" width="128" height="32"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Tilemap/OpenableDoor.tsx
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="OpenableDoor" tilewidth="16" tileheight="16" tilecount="40" columns="10">
+ <image source="../../../../../OneDrive/Code Crusade Resources/Newgfx/OpenableDoor.png" width="160" height="64"/>
+ <tile id="8">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="13" width="16" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="13" width="16" height="2"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="13" width="16" height="2"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
