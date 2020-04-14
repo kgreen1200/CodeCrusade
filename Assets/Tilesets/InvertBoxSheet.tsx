13315caf0ebf42e00ce7e8053e22992f939b1b70
Ian Higa <finn2252000@gmail.com>
2020-04-14 10:45:01 -0700

    Added tilesets
    Openable door, NOT gate, NOT gate box

---
+++ Assets/Tilesets/InvertBoxSheet.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="InvertBoxSheet" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../../../../OneDrive/Code Crusade Resources/Newgfx/InvertBoxSheet.png" width="128" height="128"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <animation>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="32" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="46">
+  <animation>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="40" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="54">
+  <animation>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="55">
+  <animation>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="48" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Tilesets/InverterWireSheet.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="InverterWireSheet" tilewidth="16" tileheight="16" tilecount="36" columns="9">
+ <image source="../../../../../OneDrive/Code Crusade Resources/Newgfx/InverterWireSheet.png" width="144" height="64"/>
+ <tile id="7">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="26">
+  <animation>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="18" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <animation>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="35">
+  <animation>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="27" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
